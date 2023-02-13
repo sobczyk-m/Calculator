4461e173f292064a422c2d4972e8bbb1d36c81c4
Marcin Sobczyk <marcin.sobczyk.dev@gmail.com>
2023-02-13 14:44:01 +0100

    fix: change background of popup parentheses

--- src/App.js
+++ src/App.js
@@ -168,24 +168,35 @@ function App() {
 
         leftParenthesis.id = "("
         leftParenthesis.innerText = "("
+        leftParenthesis.style.width = "50%"
+        leftParenthesis.style.height = "100%"
+        leftParenthesis.style.backgroundColor = buttonStyle.default.backgroundColor
+        leftParenthesis.style.border = `0px solid ${buttonStyle.default.backgroundColor}`
+        leftParenthesis.style.borderRight = `1px solid #adb5bd`
+        leftParenthesis.style.borderRadius = `10px`
 
         rightParenthesis.id = ")"
         rightParenthesis.innerText = ")"
-
-        leftParenthesis.style.width = "50%"
         rightParenthesis.style.width = "50%"
-        leftParenthesis.style.height = "100%"
         rightParenthesis.style.height = "100%"
+        rightParenthesis.style.backgroundColor = buttonStyle.default.backgroundColor
+        rightParenthesis.style.border = `0px solid ${buttonStyle.default.backgroundColor}`
+        rightParenthesis.style.borderLeft = `1px solid #adb5bd`
+        rightParenthesis.style.borderRadius = `10px`
 
         leftParenthesis.addEventListener("click", ev => onParenthesisPick(leftParenthesis.id))
         rightParenthesis.addEventListener("click", ev => onParenthesisPick(rightParenthesis.id))
-
         document.getElementById("parenthesis").innerText = ""
+
+        document.getElementById("parenthesis").style.backgroundColor = "#adb5bd"
+        document.getElementById("parenthesis").style.border = "none"
         document.getElementById("parenthesis").append(leftParenthesis)
         document.getElementById("parenthesis").append(rightParenthesis)
 
         document.getElementById("parenthesis").onmouseleave =
             () => {
+                document.getElementById("parenthesis").style.backgroundColor = buttonStyle.default.backgroundColor
+                document.getElementById("parenthesis").style.border = `1px solid ${buttonStyle.default.backgroundColor}`
                 document.getElementById("parenthesis").innerText = "( )"
             }
     }
