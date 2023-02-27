89836d35156ff8cf95d73ceaeea25177f5261521
Marcin Sobczyk <marcin.sobczyk.dev@gmail.com>
2023-02-27 12:33:35 +0100

    fix: correct percentage insertion in expressions

--- src/App.js
+++ src/App.js
@@ -139,8 +139,44 @@ function App() {
         setDisplayResult(false)
         if (activeOperator === "%") {
             if (numbers.includes(lastDisplayChar) || lastDisplayChar === ")") {
-                setMathExpression(mathExpression + operators[activeOperator])
-                setDisplayExpression((displayExpression + activeOperator))
+                let countDisplayParenthesis = 0
+                let countMathParenthesis = 0
+
+                for (let i = displayExpression.length - 1; i >= 0; i--) {
+                    if (displayExpression[i] === ")") {
+                        if ((numbers.includes(displayExpression[i + 1]) || displayExpression[i + 1] === "(")
+                            && countDisplayParenthesis === 0) {
+                            setDisplayExpression(displayExpression.slice(0, i + 1) + "("
+                                + displayExpression.slice(i + 1) + activeOperator + ")")
+                            break
+                        } else {
+                            countDisplayParenthesis++
+                        }
+                    } else if (displayExpression[i] === "(") {
+                        countDisplayParenthesis--
+                    } else if ((Object.keys(operators).includes(displayExpression[i])
+                            || (numbers.includes(displayExpression[i]) && displayExpression[i + 1] === "("))
+                        && countDisplayParenthesis === 0) {
+                        setDisplayExpression(displayExpression.slice(0, i + 1) + "("
+                            + displayExpression.slice(i + 1) + activeOperator + ")")
+                        break
+                    } else {
+                        setDisplayExpression((displayExpression + activeOperator))
+                    }
+                }
+
+                for (let i = mathExpression.length - 1; i >= 0; i--) {
+                    if (mathExpression[i] === ")") {
+                        countMathParenthesis++
+                    } else if (mathExpression[i] === "(") {
+                        countMathParenthesis--
+                    } else if (Object.values(operators).filter(value => value !== "/100").includes(mathExpression[i]) && countMathParenthesis === 0) {
+                        setMathExpression(mathExpression.slice(0, i + 1) + "(" + mathExpression.slice(i + 1) + operators[activeOperator] + ")")
+                        break
+                    } else {
+                        setMathExpression(mathExpression + operators[activeOperator])
+                    }
+                }
             } else return null
         } else if (lastDisplayChar === activeOperator ||
             ((lastDisplayChar === "(" || displayExpression === "" || displayExpression === "-" || displayExpression === "+")
