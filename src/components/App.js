e8ef591c461d07c7afe0aaf1bff9081ddaf06ef7
Marcin Sobczyk <marcin.sobczyk.dev@gmail.com>
2023-03-04 10:27:31 +0100

    Revert "refactor: extract functions from App component to separate files"
    This reverts commit ade782e5a4ce3b5bc0413dbd6549d2097791999e.

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,13 @@
-import {useEffect, useState} from "react"
+import {useCallback, useEffect, useState} from "react"
 import Display from "./Display"
 import Buttons from "./Buttons"
-import {buttons, numbers, operators} from "../data/buttonsCollection"
-import handleNumberClick from "../utils/handleNumberClick"
-import handleOperatorClick from "../utils/handleOperatorClick"
-import handleClearClick from "../utils/handleClearClick"
-import handleDeleteClick from "../utils/handleDeleteClick"
-import handleDotClick from "../utils/handleDotClick"
+import {buttons, numbers, operators, parentheses} from "../data/buttonsCollection"
 import buttonStyle from "../data/buttonStyle"
-import handleParenthesisPick from "../utils/handleParenthesisPick"
-import handleParenthesisClick from "../utils/handleParenthesisClick"
-import handleEqualsClick from "../utils/handleEqualsClick"
+
+const historyResultStyle = {
+    correct: {"color": "#ffa500"},
+    incorrect: {"color": "red"}
+}
 
 const setButtonStyle = (sign) => {
     switch (true) {
@@ -43,82 +40,320 @@ function App() {
     const penultimateDisplayChar = displayExpression[displayExpression.length - 2]
 
     const handleKeyDown = (e) => {
+        console.log(e.key)
         switch (true) {
             case buttons.some(value => value.sign === e.key && e.key !== "x" && e.key !== "C"):
-                handleButtonClick(e.key)
+                onButtonClick(e.key)
                 break
             case e.key === "Enter": {
-                handleEqualsClick(displayExpression, setDisplayExpression, mathExpression,
-                    setMathExpression, historyExpression, setHistoryExpression, setDisplayResult)
+                onButtonClick("=")
                 break
             }
             case e.key === "*":
-                handleOperatorClick("x", lastDisplayChar, displayExpression, setDisplayExpression, mathExpression, setMathExpression,
-                    displayResult, setDisplayResult)
+                onOperatorClick("x")
                 break
             case e.key === "Delete": {
-                handleClearClick(setDisplayResult, setDisplayExpression, mathExpression, setMathExpression,
-                    setHistoryExpression)
+                onButtonClick("C")
                 break
             }
             case e.key === "Backspace": {
-                handleDeleteClick(penultimateDisplayChar, lastDisplayChar, displayExpression, setDisplayExpression,
-                    mathExpression, setMathExpression, setDisplayResult)
+                onButtonClick("del")
                 break
             }
             case e.key === "(":
-                handleParenthesisPick("(", lastDisplayChar, displayExpression, setDisplayExpression,
-                    mathExpression, setMathExpression, setDisplayResult)
+                onParenthesisPick("(")
                 break
             case e.key === ")":
-                handleParenthesisPick(")", lastDisplayChar, displayExpression, setDisplayExpression,
-                    mathExpression, setMathExpression, setDisplayResult)
+                onParenthesisPick(")")
                 break
             default:
                 break
         }
     }
 
-    const handleButtonClick = (buttonSign) => {
+    const onNumberClick = (number) => {
+        if (displayResult === true) {
+            setMathExpression(number)
+            setDisplayExpression(number)
+            setDisplayResult(false)
+        } else if (lastDisplayChar === "%" || lastDisplayChar === ")") {
+            setMathExpression(mathExpression + "*" + number)
+            setDisplayExpression(displayExpression + number)
+        } else {
+            setMathExpression(mathExpression + number)
+            setDisplayExpression(displayExpression + number)
+        }
+    }
+
+    const onClearClick = () => {
+        setDisplayResult(false)
+        if (mathExpression === "") {
+            setDisplayExpression("")
+            setMathExpression("")
+            setHistoryExpression([])
+        } else {
+            setMathExpression("")
+            setDisplayExpression("")
+        }
+    }
+
+    const onDeleteClick = () => {
+        setDisplayResult(false)
+        switch (true) {
+            case Object.keys(numbers).includes(lastDisplayChar) &&
+            (displayExpression[displayExpression.length - 2] === ")" || penultimateDisplayChar === "%"):
+                setMathExpression(mathExpression.slice(0, mathExpression.length - 2))
+                setDisplayExpression(displayExpression.slice(0, displayExpression.length - 1))
+                break
+            case Object.keys(parentheses).includes(lastDisplayChar)
+            && (Object.keys(numbers).includes(penultimateDisplayChar)
+                || penultimateDisplayChar === "%"
+                || penultimateDisplayChar === ")") !== false:
+                setMathExpression(mathExpression.slice(0,
+                    mathExpression.length - parentheses[lastDisplayChar].length))
+                setDisplayExpression(displayExpression.slice(0, displayExpression.length - 1))
+                break
+            case Object.keys(operators).includes(lastDisplayChar) :
+                setMathExpression(mathExpression.slice(0,
+                    mathExpression.length - operators[lastDisplayChar].length))
+                setDisplayExpression(displayExpression.slice(0, displayExpression.length - 1))
+                break
+            default:
+                setMathExpression(mathExpression.slice(0, mathExpression.length - 1))
+                setDisplayExpression(displayExpression.slice(0, displayExpression.length - 1))
+                break
+        }
+    }
+
+    const onOperatorClick = (sign) => {
+        const activeOperator = sign
+        const inactiveOperators = {...operators}
+        delete inactiveOperators[activeOperator]
+
+        setDisplayResult(false)
+        if (activeOperator === "%") {
+            if (numbers.includes(lastDisplayChar) || lastDisplayChar === ")") {
+                let countDisplayParenthesis = 0
+                let countMathParenthesis = 0
+
+                for (let i = displayExpression.length - 1; i >= 0; i--) {
+                    if (displayExpression[i] === ")") {
+                        if ((numbers.includes(displayExpression[i + 1]) || displayExpression[i + 1] === "(")
+                            && countDisplayParenthesis === 0) {
+                            setDisplayExpression(displayExpression.slice(0, i + 1) + "("
+                                + displayExpression.slice(i + 1) + activeOperator + ")")
+                            break
+                        } else {
+                            countDisplayParenthesis++
+                        }
+                    } else if (displayExpression[i] === "(") {
+                        countDisplayParenthesis--
+                    } else if ((Object.keys(operators).includes(displayExpression[i])
+                            || (numbers.includes(displayExpression[i]) && displayExpression[i + 1] === "("))
+                        && countDisplayParenthesis === 0) {
+                        setDisplayExpression(displayExpression.slice(0, i + 1) + "("
+                            + displayExpression.slice(i + 1) + activeOperator + ")")
+                        break
+                    } else {
+                        setDisplayExpression((displayExpression + activeOperator))
+                    }
+                }
+
+                for (let i = mathExpression.length - 1; i >= 0; i--) {
+                    if (mathExpression[i] === ")") {
+                        countMathParenthesis++
+                    } else if (mathExpression[i] === "(") {
+                        countMathParenthesis--
+                    } else if (Object.values(operators).filter(value => value !== "/100").includes(mathExpression[i]) && countMathParenthesis === 0) {
+                        setMathExpression(mathExpression.slice(0, i + 1) + "(" + mathExpression.slice(i + 1) + operators[activeOperator] + ")")
+                        break
+                    } else {
+                        setMathExpression(mathExpression + operators[activeOperator])
+                    }
+                }
+            } else return null
+        } else if (lastDisplayChar === activeOperator ||
+            ((lastDisplayChar === "(" || displayExpression === "" || displayExpression === "-" || displayExpression === "+")
+                && (activeOperator === "x" || activeOperator === "/"))) {
+            return null
+        } else if (Object.keys(inactiveOperators).includes(lastDisplayChar)
+            && lastDisplayChar !== "%") {
+            setMathExpression(mathExpression.slice(0, mathExpression.length - 1) + operators[activeOperator])
+            setDisplayExpression((displayExpression.slice(0, displayExpression.length - 1) + activeOperator))
+        } else {
+            setMathExpression(mathExpression + operators[activeOperator])
+            setDisplayExpression(displayExpression + activeOperator)
+        }
+    }
+
+    const onDotClick = () => {
+        setDisplayResult(false)
+        if (numbers.includes(lastDisplayChar)) {
+            setMathExpression(mathExpression + ".")
+            setDisplayExpression(displayExpression + ".")
+        } else return null
+    }
+
+    const onParenthesisClick = () => {
+        const leftParenthesis = document.createElement("button")
+        const rightParenthesis = document.createElement("button")
+
+        leftParenthesis.id = "("
+        leftParenthesis.textContent = "("
+        leftParenthesis.style.width = "50%"
+        leftParenthesis.style.height = "100%"
+        leftParenthesis.style.backgroundColor = buttonStyle.default.backgroundColor
+        leftParenthesis.style.border = `0px solid ${buttonStyle.default.backgroundColor}`
+        leftParenthesis.style.borderRight = `1px solid #adb5bd`
+        leftParenthesis.style.borderRadius = `10px`
+
+        rightParenthesis.id = ")"
+        rightParenthesis.textContent = ")"
+        rightParenthesis.style.width = "50%"
+        rightParenthesis.style.height = "100%"
+        rightParenthesis.style.backgroundColor = buttonStyle.default.backgroundColor
+        rightParenthesis.style.border = `0px solid ${buttonStyle.default.backgroundColor}`
+        rightParenthesis.style.borderLeft = `1px solid #adb5bd`
+        rightParenthesis.style.borderRadius = `10px`
+
+        leftParenthesis.addEventListener("click", ev => onParenthesisPick(leftParenthesis.id))
+        rightParenthesis.addEventListener("click", ev => onParenthesisPick(rightParenthesis.id))
+        document.getElementById("parenthesis").innerText = ""
+
+        document.getElementById("parenthesis").style.backgroundColor = "#adb5bd"
+        document.getElementById("parenthesis").style.border = "none"
+        document.getElementById("parenthesis").append(leftParenthesis)
+        document.getElementById("parenthesis").append(rightParenthesis)
+
+        document.getElementById("parenthesis").onmouseleave =
+            () => {
+                document.getElementById("parenthesis").style.backgroundColor = buttonStyle.default.backgroundColor
+                document.getElementById("parenthesis").style.border = `1px solid ${buttonStyle.default.backgroundColor}`
+                document.getElementById("parenthesis").innerText = "( )"
+            }
+    }
+
+    const onParenthesisPick = (sign) => {
+        switch (sign) {
+            case "(":
+                setDisplayResult(false)
+                if (numbers.includes(lastDisplayChar)
+                    || lastDisplayChar === "%"
+                    || lastDisplayChar === ")"
+                ) {
+                    setMathExpression(mathExpression + parentheses[sign])
+                    setDisplayExpression(displayExpression + sign)
+                } else {
+                    setMathExpression(mathExpression + sign)
+                    setDisplayExpression(displayExpression + sign)
+                }
+                break
+            case ")":
+                setDisplayResult(false)
+                if ((Object.keys(operators).includes(lastDisplayChar)
+                    && lastDisplayChar !== "%") || displayExpression.length < 1) {
+                    return null
+                } else {
+                    setMathExpression(mathExpression + sign)
+                    setDisplayExpression(displayExpression + sign)
+                }
+                break
+            default:
+                throw new Error("Unrecognized character, no action to perform")
+        }
+        document.getElementById("parenthesis").style.backgroundColor = buttonStyle.default.backgroundColor
+        document.getElementById("parenthesis").style.border = `1px solid ${buttonStyle.default.backgroundColor}`
+        document.getElementById("parenthesis").innerHTML = "( )"
+    }
+
+    const onEqualsClick = () => {
+        if (
+            Object.keys(operators).some(operator => displayExpression.includes(operator))
+            || Object.keys(parentheses).some(operator => displayExpression.includes(operator))
+        ) {
+            try {
+                const result = Function('return ' + mathExpression)()
+                const redundantPartOfResult = /[.]*0+$/
+
+                setDisplayExpression(result.toFixed(8).toString().replace(redundantPartOfResult, ""))
+                setMathExpression(result.toFixed(8).toString().replace(redundantPartOfResult, ""))
+                setDisplayResult(true)
+
+                if (historyExpression.length > 0) {
+                    return historyExpression[0].result.includes("Error") ?
+                        setHistoryExpression([{
+                            historyExpression: displayExpression,
+                            result: result.toFixed(8).toString().replace(redundantPartOfResult, "")
+                        }, ...historyExpression.slice(1)]) :
+                        setHistoryExpression([{
+                            historyExpression: displayExpression,
+                            result: result.toFixed(8).toString().replace(redundantPartOfResult, "")
+                        }, ...historyExpression])
+                } else return setHistoryExpression([{
+                    historyExpression: displayExpression,
+                    result: result.toFixed(8).toString().replace(redundantPartOfResult, "")
+                }, ...historyExpression])
+            } catch (e) {
+                if (historyExpression.length > 0) {
+                    return historyExpression[0].result.includes("Error") ?
+                        setHistoryExpression([{
+                            historyExpression: displayExpression,
+                            result: "Expression Error"
+                        }, ...historyExpression.slice(1)]) :
+                        setHistoryExpression([{
+                            historyExpression: displayExpression,
+                            result: "Expression Error"
+                        }, ...historyExpression])
+                } else return setHistoryExpression([{
+                    historyExpression: displayExpression,
+                    result: "Expression Error"
+                }, ...historyExpression])
+            }
+        }
+    }
+
+    const onButtonClick = (buttonSign) => {
         switch (true) {
             case numbers.includes(buttonSign):
-                handleNumberClick(buttonSign, lastDisplayChar, displayExpression, setDisplayExpression, mathExpression,
-                    setMathExpression, displayResult, setDisplayResult)
+                onNumberClick(buttonSign)
                 break
             case Object.keys(operators).includes(buttonSign):
-                handleOperatorClick(buttonSign, lastDisplayChar, displayExpression, setDisplayExpression, mathExpression,
-                    setMathExpression, displayResult, setDisplayResult)
+                onOperatorClick(buttonSign)
                 break
             case buttonSign === "C":
-                handleClearClick(setDisplayResult, setDisplayExpression, mathExpression, setMathExpression,
-                    setHistoryExpression)
+                onClearClick()
                 break
             case buttonSign === "del":
-                handleDeleteClick(penultimateDisplayChar, lastDisplayChar, displayExpression, setDisplayExpression,
-                    mathExpression, setMathExpression, setDisplayResult)
+                onDeleteClick()
                 break
             case buttonSign === ".":
-                handleDotClick(lastDisplayChar, displayExpression, setDisplayExpression, mathExpression, setMathExpression,
-                    setDisplayResult)
+                onDotClick()
                 break
             case buttonSign === "( )":
-                handleParenthesisClick(lastDisplayChar, displayExpression, setDisplayExpression,
-                    mathExpression, setMathExpression, setDisplayResult)
+                onParenthesisClick()
                 break
             case buttonSign === "=":
-                handleEqualsClick(displayExpression, setDisplayExpression, mathExpression,
-                    setMathExpression, historyExpression, setHistoryExpression, setDisplayResult)
+                onEqualsClick()
                 break
             default:
                 throw new Error("Unrecognized character, no action to perform")
         }
     }
 
+    const createHistoryExpression = useCallback(() => {
+        return historyExpression.map((ele, index) =>
+            <div className={"historyCalculationWrapper"} key={historyExpression.indexOf(ele)}>
+                <span data-testid={"historyExpression #" + index}>{ele.historyExpression}</span>
+                <span>{"="}</span>
+                <span data-testid={"result #" + index} style={ele.result.includes("Error") ?
+                    historyResultStyle.incorrect : historyResultStyle.correct}>{ele.result}</span>
+            </div>)
+    }, [historyExpression])
+
     return (
         <div id={"calculator"}>
-            <Display historyExpression={historyExpression}
-                     displayExpression={displayExpression}/>
-            <Buttons buttons={buttons} buttonStyle={setButtonStyle} onButtonClick={handleButtonClick}/>
+            <Display historyExpression={createHistoryExpression()} displayExpression={displayExpression}/>
+            <Buttons buttons={buttons} buttonStyle={setButtonStyle} onButtonClick={onButtonClick}/>
         </div>
     )
 }
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,15 +1,13 @@
 function Buttons(props) {
-    const {buttons, buttonStyle, onButtonClick} = props
 
     const createButton = () => {
-        return buttons.map(button => <div id={button.name} key={button.name} className={"button"}
-                                          style={buttonStyle(button.sign)}
-                                          onClick={() => onButtonClick(button.sign)}>{button.sign}</div>)
+        return props.buttons.map(button => <div id={button.name} key={button.name} className={"button"}
+                                                style={props.buttonStyle(button.sign)}
+                                                onClick={() => props.onButtonClick(button.sign)}>{button.sign}</div>)
     }
 
     return (
-        <div id={"buttons-container"}>{createButton()}</div>
-    )
+        <div id={"buttons-container"}>{createButton()}</div>)
 }
 
 export default Buttons
